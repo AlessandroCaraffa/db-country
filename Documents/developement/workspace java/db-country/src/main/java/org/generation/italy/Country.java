c98c3b6f972c388e1d01cb1559279f29715cd590
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-12-22 15:29:20 +0100

    classe country creata,main iniziato

--- Documents/developement/workspace java/db-country/src/main/java/org/generation/italy/Country.java
+++ Documents/developement/workspace java/db-country/src/main/java/org/generation/italy/Country.java
@@ -1,5 +1,113 @@
 package org.generation.italy;
 
-public class Country {
+import java.time.LocalDate;
 
+public class Country {
+	private int countryId;
+	private String name;
+	private int area;
+	private LocalDate nationalDay;
+	private String countryCode;
+	private String countryCode2;
+	private int regionId;
+	
+	
+	
+	public Country (int countryId ,String name, int area ,LocalDate nationalDay ,  String countryCode , String countryCode2 , int regionId) {
+		this.countryId = countryId;
+		this.name = name;
+		this.area=area;
+		this.nationalDay = nationalDay;
+		this.countryCode = countryCode;
+		this.countryCode2 = countryCode2;
+		this.regionId = regionId;
+	}
+
+
+
+	public int getCountryId() {
+		return countryId;
+	}
+
+
+
+	public void setCountryId(int countryId) {
+		this.countryId = countryId;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public int getArea() {
+		return area;
+	}
+
+
+
+	public void setArea(int area) {
+		this.area = area;
+	}
+
+
+
+	public LocalDate getNationalDay() {
+		return nationalDay;
+	}
+
+
+
+	public void setNationalDay(LocalDate nationalDay) {
+		this.nationalDay = nationalDay;
+	}
+
+
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+
+
+	public String getCountryCode2() {
+		return countryCode2;
+	}
+
+
+
+	public void setCountryCode2(String countryCode2) {
+		this.countryCode2 = countryCode2;
+	}
+
+
+
+	public int getRegionId() {
+		return regionId;
+	}
+
+
+
+	public void setRegionId(int regionId) {
+		this.regionId = regionId;
+	}
+
+	
+	
+	
 }
--- Documents/developement/workspace java/db-country/src/main/java/org/generation/italy/Main.java
+++ Documents/developement/workspace java/db-country/src/main/java/org/generation/italy/Main.java
@@ -1,9 +1,27 @@
 package org.generation.italy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+	private final static String DB_URL = "jdbc:mysql://localhost:3306/nomeDataBase";				
+	private final static String DB_USER = "root";
+	private final static String DB_PASSWORD = "rootpassword";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try(Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+			
+		
+		
+		
+		
+		
+		}catch (SQLException e) {
+			System.out.println("OOPS an error as occured");
+			System.out.println(e.getMessage());
+		}
+		
 
 	}
 
